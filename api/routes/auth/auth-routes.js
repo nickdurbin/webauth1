aa4bbf8e3b5dadf09b7bbf54d42787ae4f67c432
ndurbin <nick.durbin@gmail.com>
2020-01-14 22:16:42 -0500

    Changed login and register to auth routes.

---
+++ api/routes/auth/auth-routes.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const bcrypt = require('bcryptjs')
+const Users = require("../users/users-model")
+const router = express.Router()
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const saved = await Users.add(req.body)
+    
+    res.status(201).json(saved)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await Users.findBy({ username }).first()
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (user && passwordValid === user.password) {
+      res.status(200).json({
+        message: `Welcome ${user.username}!`,
+      })
+    } else {
+      res.status(401).json({
+        message: "Invalid Credentials",
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
